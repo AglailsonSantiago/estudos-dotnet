5c0d80385009a4f7013ad4a452a2244cf44edb03
unknown <aglailsonsantiago@gmail.com>
2022-08-04 12:01:18 -0300

    Configurando propriedades e relacionamento.

--- MinimalApiCatalogo/Context/AppDbContext.cs
+++ MinimalApiCatalogo/Context/AppDbContext.cs
@@ -10,5 +10,26 @@ namespace MinimalApiCatalogo.Context
 
         public DbSet<Produto> Produtos { get; set; }
         public DbSet<Categoria> Categorias { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder mb)
+        {   
+            //categoria
+            mb.Entity<Categoria>().HasKey(c => c.CategoriaId);
+            mb.Entity<Categoria>().Property(c => c.Nome).HasMaxLength(100).IsRequired();
+            mb.Entity<Categoria>().Property(c => c.Descricao).HasMaxLength(150).IsRequired();
+
+            //produto
+            mb.Entity<Produto>().HasKey(c => c.ProdutoId);
+            mb.Entity<Produto>().Property(c => c.Nome).HasMaxLength(100).IsRequired();
+            mb.Entity<Produto>().Property(c => c.Descricao).HasMaxLength(150).IsRequired();
+            mb.Entity<Produto>().Property(c => c.Imagem).HasMaxLength(100).IsRequired();
+            mb.Entity<Produto>().Property(c => c.Preco).HasPrecision(14, 2);
+
+            //relacionamento categoria-produto
+            mb.Entity<Produto>()
+                .HasOne<Categoria>(c => c.Categoria)
+                    .WithMany(p => p.Produtos)
+                        .HasForeignKey(c => c.CategoriaId);
+        }
     }
 }
